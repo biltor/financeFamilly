732af91848f55396fca25436de0d08a2b0b40aea
Torche Bilel <bil.tor@gmail.com>
2026-01-22 19:41:22 +0100

    add user and make it with client

--- database/migrations/2026_01_19_082805_create_clients_table.php
+++ database/migrations/2026_01_19_082805_create_clients_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->foreignId('fonction_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('user_id')->unique()->constrained()->cascadeOnDelete()->nullable();
+            $table->foreignId('user_id')->nullable()->unique()->constrained()->nullOnDelete();
             $table->timestamps();
         });
     }
